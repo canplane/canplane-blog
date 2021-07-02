917ef69cb2a7b48345fc1cbad0db5c7497e2306b
canplane <canplane@gmail.com>
2021-07-03 05:10:39 +0900

    2107030510

--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -3,19 +3,15 @@ import React from 'react';
 import './style.scss';
 
 const PageFooter = ({ author, githubUrl }) => (
-  <>
-    <footer className="page-footer-wrapper">
-      <p className="page-footer">
-        © {new Date().getFullYear()}
-        &nbsp;
-        <a href={githubUrl}>{author}</a>
-        &nbsp;powered by
-        <a href="https://github.com/zoomKoding/gatsby-starter-zoomkoding">
-          &nbsp;gatsby-starter-zoomkoding
-        </a>
-      </p>
-    </footer>
-  </>
+  <footer className="page-footer-wrapper">
+    <p className="page-footer">
+      © {new Date().getFullYear()}{/* <a href={githubUrl}>{author}</a>*/}
+      &nbsp;|&nbsp;
+      Built with <a href="https://www.gatsbyjs.com/">Gatsby</a>
+      &nbsp;|&nbsp;
+      Theme by <a href="https://github.com/zoomKoding/gatsby-starter-zoomkoding">zoomkoding</a>
+    </p>
+  </footer>
 );
 
 export default PageFooter;
