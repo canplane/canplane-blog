ff19b56e1448f3af2627fee2f9a3b654ce87123a
canplane <canplane@gmail.com>
2021-07-03 11:30:14 +0900

    2107031130

--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -6,9 +6,11 @@ const PageFooter = ({ author, githubUrl }) => (
   <>
     <footer className="page-footer-wrapper">
       <p className="page-footer">
-        © {new Date().getFullYear()}{/* <a href={githubUrl}>{author}</a>*/}
-        &nbsp;| Built with <a href="https://www.gatsbyjs.com/">Gatsby</a>
-        &nbsp;| Designed by <a href="https://github.com/zoomKoding/gatsby-starter-zoomkoding">zoomkoding</a> &amp; <a href="https://cactus.tistory.com/306">orioncactus</a>
+        © {new Date().getFullYear()} <a href={githubUrl}>{author}</a>
+        <br />
+        Built with <a href="https://www.gatsbyjs.com/">Gatsby</a>
+        &nbsp;| Theme by <a href="https://github.com/zoomKoding/gatsby-starter-zoomkoding">zoomkoding</a>
+        &nbsp;| Font by <a href="https://cactus.tistory.com/306">orioncactus</a>
       </p>
     </footer>
   </>
